f8c15e5866120f27d7fc996e453cc4e9b2c5be43
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-06 19:29:35 +0400

    to upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if (ord(str) >= 65 and ord(str) <= 90):
-        return True
-    return False
+    for i in str:
+        if (ord(i) >= 97 and ord(i) <= 122):
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
