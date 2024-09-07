7382720da42ae59288e664ae4b3d65c236989fbf
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-07 18:56:15 +0400

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if (n == 0):
+        print("{} arguments.".format(n))
+    else:
+        print("{} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))
