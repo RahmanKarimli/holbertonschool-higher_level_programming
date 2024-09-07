d898d517519f579cdfff96ee85dcc44c467a8dc1
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-07 18:15:40 +0400

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-res = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, res))
 if __name__ == "__main__":
-	add_0.add()
+    a = 1
+    b = 2
+    res = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, res))
