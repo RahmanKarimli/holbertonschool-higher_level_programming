99f01fa951e1834370528ca0c08b0d111242bde0
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-10 00:29:25 +0400

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -19,7 +19,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tmp[0] = tuple_a[0]
         tmp[1] = tuple_a[1]
 
-    if (len(tuple_a) == 2 and len(tuple_b) == 2):
+    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
         tmp[0] = tuple_a[0]
         tmp[1] = tuple_a[1]
         tmp[2] = tuple_b[0]
