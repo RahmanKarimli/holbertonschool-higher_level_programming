8e644124e3643cf0091f3c7f83939a4c720f829a
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-10 00:39:03 +0400

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tmp = [0, 0, 0, 0]
-    if (len(tuple_a) == 1):
-        tmp[0] = tuple_a[0]
-        tmp[2] = tuple_b[0]
-        tmp[3] = tuple_b[1]
+    tmp = [0, 0]
+    la = len(tuple_a)
+    lb = len(tuple_b)
 
-    if (len(tuple_b) == 1):
+    if (la > 0):
         tmp[0] = tuple_a[0]
+    if (la > 1):
         tmp[1] = tuple_a[1]
-        tmp[2] = tuple_b[0]
+    if (lb > 0):
+        tmp[0] += tuple_b[0]
+    if (lb > 1):
+        tmp[1] += tuple_b[1]
 
-    if (len(tuple_a) == 0):
-        tmp[2] = tuple_b[0]
-        tmp[3] = tuple_b[1]
-
-    if (len(tuple_b) == 0):
-        tmp[0] = tuple_a[0]
-        tmp[1] = tuple_a[1]
-
-    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
-        tmp[0] = tuple_a[0]
-        tmp[1] = tuple_a[1]
-        tmp[2] = tuple_b[0]
-        tmp[3] = tuple_b[1]
-    new = (tmp[0] + tmp[2], tmp[1] + tmp[3])
+    new = (tmp[0], tmp[1])
     return new
