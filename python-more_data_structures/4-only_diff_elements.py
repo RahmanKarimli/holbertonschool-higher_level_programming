eba3fa8fcc969bd805be6594cd316a8003d1e8f3
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-11 15:56:52 +0400

    only different elements

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new = set()
-    for i in set_2:
-        if (i not in set_1):
-            new.add(i)
-    return new
+    return set_1.symmetric_difference(set_2)
