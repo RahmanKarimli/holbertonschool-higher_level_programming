402149642bc9731313fe34a072e551e88e2bb9b3
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-14 17:42:46 +0400

    safe print list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[num], end="")
             num += 1
-        print()
     except:
-        print()
+        pass
+    print()
     return num
