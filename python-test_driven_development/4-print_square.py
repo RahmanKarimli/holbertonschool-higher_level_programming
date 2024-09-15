6095b66ddc49a1bdab949397b5b4b01704757e6d
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-09-15 22:14:27 +0400

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,12 +4,12 @@
 
 def print_square(size):
     """print square"""
-    if (type(size) != int):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if (size < 0):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
         for j in range(size):
-            print("#",end="")
+            print("#", end="")
         print()
