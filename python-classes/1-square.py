d5757e6e95f3c36977fcdf27b62a76c86344b670
Your Name <rahman.karimli@icloud.com>
2024-09-16 18:23:34 +0400

    Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """instance attribute: size"""
+    def __init__(self, size):
+        self.__size = size
