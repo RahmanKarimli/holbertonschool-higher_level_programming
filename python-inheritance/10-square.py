e198f9ed02cfd5f1627932c791bd45effcb726fa
Your Name <you@example.com>
2024-09-23 19:10:25 +0400

    10 square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """class"""
-BaseGeometry = __import__('9-rectangle.py').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """class"""
     def __init__(self, size):
-        BaseGeometry.integer_validator(self, "size", size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
