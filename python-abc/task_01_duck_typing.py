2203ed62814f607f03ed32088c0a1b2215e3dfd1
Your Name <you@example.com>
2024-09-27 18:02:58 +0400

    comit

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""class"""
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """class"""
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    """class"""
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return pi * self.radius * 2
+
+class Rectangle(Shape):
+    """Class"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(cls):
+    print(cls.area())
+    print(cls.perimeter())
