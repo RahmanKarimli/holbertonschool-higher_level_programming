93622307a75ad4d85837d4827cf34d37d2a02b9f
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-10-05 23:16:21 +0400

    pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""python"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    matrix = [[1]]
+    x = 0
+    for k in range(n - 1):
+        tmp = []
+        j = 0
+        tmp.append(1)
+        for i in range(1, len(matrix[x])):
+            tmp.append(matrix[x][j] + matrix[x][i])
+            j += 1
+        x += 1
+        tmp.append(1)
+        matrix.append(tmp)
+    return matrix
