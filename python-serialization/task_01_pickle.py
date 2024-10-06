881a0c4d17f7dfd6e33fd26c79909036bfaa23bb
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-10-06 19:54:49 +0400

    pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,22 @@
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Students: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
