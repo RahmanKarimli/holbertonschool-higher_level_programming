3dab19ad915f29ff73d01dedf783d4332ce7c8c4
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-03 19:33:49 +0400

    com

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-SQL
+Connecting to database and listing it
 """
 
 if __name__ == "__main__":
@@ -8,20 +8,22 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-    cursor = db.cursor()
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+    query = """SELECT * FROM states WHERE name LIKE BINARY "N%" ORDER BY id"""
     try:
-        cursor.execute("""SELECT * FROM states WHERE name LIKE "N%" ORDER BY id""")
-        rows = cursor.fetchall()
+        cur.execute(query)
+        rows = cur.fetchall()
     except MySQLdb.Error as e:
         print(e)
 
-    for i in rows:
-        print(i)
-    cursor.close()
+    for row in rows:
+        print(row)
+
+    cur.close()
     db.close()
