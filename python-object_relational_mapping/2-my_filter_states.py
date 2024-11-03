495ece70d97514b8bfdfff3cf2f8acf113371970
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-03 19:45:37 +0400

    filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-SQL
+Connecting to database and listing it
 """
 
 if __name__ == "__main__":
@@ -8,23 +8,26 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-    cursor = db.cursor()
-    query = """SELECT * FROM states WHERE name LIKE "{:s}" ORDER BY id"""
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+
     try:
-        cursor.execute(query.format(argv[4]))
-        rows = cursor.fetchall()
+        query = """
+        SELECT * FROM states WHERE name='{:s}' ORDER BY states.id
+        """
+        cur.execute(query.format(argv[4]))
+        rows = cur.fetchall()
     except MySQLdb.Error as e:
         print(e)
 
-    for i in rows:
+    for row in rows:
         if row[1] == argv[4]:
             print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
