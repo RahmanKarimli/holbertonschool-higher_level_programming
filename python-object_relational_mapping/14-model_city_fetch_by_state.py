02f0aa0f5764eea2fe6303ef1001d761e796c5bb
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-03 23:32:23 +0400

    model city fetch

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
-Adds Louisiana
+Cities
 """
 
 from sys import argv
@@ -13,11 +13,13 @@ from model_state import State
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         argv[1], argv[2], argv[3]
-    ), pool_pre_ping=True)
-
+        ), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    rows = session.query(City, State).filter(City.state_id == State.id).order_by(City.id)
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    rows = session.query(City, State).filter(
+            City.state_id == State.id
+            ).order_by(City.id)
+    [print("{}: ({}) {}".format(
+        state.name, city.id, city.name
+        )) for city, state in rows]
