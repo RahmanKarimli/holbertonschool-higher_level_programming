10252907f7ab7e4ddf5dc7c88ad323f5689c3ebc
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-03 23:29:59 +0400

    model city fetch state

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3
+"""
+Adds Louisiana
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_city import City
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]
+    ), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = session.query(City, State).filter(City.state_id == State.id).order_by(City.id)
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
