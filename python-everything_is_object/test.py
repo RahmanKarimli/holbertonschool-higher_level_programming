2036a57d2ac41d111d6a4f03f56f1211e97ad424
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-02 15:44:38 +0400

    answer

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -2,13 +2,33 @@
 
 a = [1,2,3]
 b = a
-b.append(4)
+a = a + [4]
 
 print(a)
 print(b)
 print(id(a)==id(b))
 print(a is b)
 print(a == b)
+
+
+print("------")
+x = [1,2,3]
+print(id(x))
+# x.append(4)
+x += [4]
+print(id(x))
+
+
+print("-----")
+def inc(n):
+    n.append(3)
+a = [1,2]
+inc(a)
+print(a)
+
+
+
+
 
 
 
