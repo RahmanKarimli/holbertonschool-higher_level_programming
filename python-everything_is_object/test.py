38d1f286f88c1882179620a4de4c4afc9cd84f87
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-12-02 16:21:43 +0400

    fix

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -39,10 +39,18 @@ print(a is b)
 
 
 print("-------")
-a = (1, 2)
-b = (1, 2)
-print(a is b)
+t = (1, 2)
+y = (1, 2)
+print(t is y)
 
+
+print("-------")
+a = [1, 2, 3]
+print(id(a))
+a.append(4)
+print(id(a))
+a += [5]
+print(id(a))
 
 
 
